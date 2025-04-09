ddde697be144c8238fa8a2ed2e96003d9f3685b2
Hussain77777 <80383610+Hussain77777@users.noreply.github.com>
2025-04-09 12:14:50 +0500

    u

---
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListState.kt
@@ -0,0 +1,12 @@
+
+import com.plcoding.bookpedia.book.domain.Book
+import com.plcoding.bookpedia.core.presentation.UiText
+
+data class BookListState(
+    val searchQuery:String = "Kotlin",
+    val searchResults:List<Book> =emptyList(),
+    val favouriteResults:List<Book> =emptyList(),
+    val isLoading:Boolean = false,
+    val selectedTabIndex:Int = 0,
+    val errorMessage:UiText?= null
+)
